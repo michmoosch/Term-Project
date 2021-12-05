461830b478f3393dd68a56596b360a9fa9bea94e
michmoosch <michael.moore01@sjsu.edu>
2021-12-04 16:18:34 -0800

    Added detailed entry view & Input validation

--- team-03-10/src/application/Main.java
+++ team-03-10/src/application/Main.java
@@ -25,6 +25,7 @@ import javax.xml.bind.Marshaller;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 import javafx.scene.Scene;
@@ -47,7 +48,9 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -58,8 +61,9 @@ import application.Entry;
 
 public class Main extends Application {
 	
-	public boolean isValidName(String s){      
-	     String regex="[A-Za-z\\s]+";      
+	// String Validator
+	public boolean isValidStr(String s){      
+	     String regex="^[a-zA-Z0-9\\s\\/.,!?]+$";      
 	      return s.matches(regex);//returns true if input and regex matches otherwise false;
 	 }
 
@@ -75,24 +79,23 @@ public class Main extends Application {
 		title.setFont(new Font("Times New Roman", 50));
 		title.setPadding(new Insets(75, 75, 0, 75));
 		
-		//1st Button
+		//Record your Journey Button
 		Button btn1 = new Button("Record Your Journey");
 		btn1.setAlignment(Pos.CENTER);
 		btn1.setFont(Font.font("Times New Roman", 20));
 		
-		//2nd Button
+		//History Button
 		Button btn2 = new Button("History");
 		btn2.setAlignment(Pos.CENTER);		
 		btn2.setFont(Font.font("Times New Roman", 20));
 		
-	
 		//Home Screen Layout
 		VBox homeScreen = new VBox();
 		homeScreen.setAlignment(Pos.TOP_CENTER);
 		homeScreen.setSpacing(100);
 		homeScreen.setStyle("-fx-background-color: #607658;");
 		
-		//Layout 1
+		//Home scene
 		homeScreen.getChildren().addAll(title, btn1, btn2);
 		Scene homeScene = new Scene(homeScreen, 1200, 800);
 		
@@ -105,10 +108,7 @@ public class Main extends Application {
 		
 		
 		//-----------------Record Your Journey Screen------------------------
-		Label label2 = new Label("Record Your Journey");
-		label2.setStyle("-fx-text-fill: white;");
-		label2.setFont(new Font("Times New Roman", 50));
-		label2.setPadding(new Insets(75, 75, 0, 75));
+		
 		
 		/**
 		 * Text Fields  
@@ -116,45 +116,51 @@ public class Main extends Application {
 		 * 			Date
 		 * 			distance
 		 * 			time elapsed
-		 * 			??? uplaod picutrees??
 		 * 			Journal entry
 		 */	
 		
-		GridPane gpforText = new GridPane();
+		//Title Label
+		Label label2 = new Label("Record Your Journey");
+		label2.setStyle("-fx-text-fill: white;");
+		label2.setFont(new Font("Times New Roman", 50));
+		label2.setPadding(new Insets(75, 75, 0, 75));
 		
+		//Gridpane to hold text boxes
+		GridPane gpforText = new GridPane();
 		gpforText.setPadding(new Insets(10, 10, 10, 10));
 		gpforText.setVgap(5);
 		gpforText.setHgap(5);
+		gpforText.setAlignment(Pos.CENTER);
 		
-		
+		//Name Text Field
 		TextField name = new TextField ();
 		name.setPromptText("Journal Name");
 		name.setPrefWidth(100);
 		gpforText.setConstraints(name, 0, 0);
 		gpforText.getChildren().add(name);
 		
-		
+		//Date Text Field
 		TextField date = new TextField ();
 		date.setPromptText("Enter Date: DD/MM/YYYY");
 		date.setPrefWidth(100);
 		gpforText.setConstraints(date, 0, 1);
 		gpforText.getChildren().add(date);
 		
-		
+		//Distance Text Field
 		TextField distance = new TextField ();
 		distance.setPromptText("Miles Traversed");
 		distance.setPrefWidth(100);
 		gpforText.setConstraints(distance, 0, 2);
 		gpforText.getChildren().add(distance);
 		
-		
+		//Time Text Field
 		TextField time = new TextField ();
 		time.setPromptText("Time Elapsed");
 		time.setPrefWidth(100);
 		gpforText.setConstraints(time, 0, 3);
 		gpforText.getChildren().add(time);
 		
-		
+		//Entry Text Field
 		TextArea entry = new TextArea ();
 		entry.setPromptText("Journal Entry");
 		entry.setWrapText(true);
@@ -163,27 +169,32 @@ public class Main extends Application {
 		gpforText.setConstraints(entry, 0, 4);
 		gpforText.getChildren().add(entry);
 		
-		// Submit text fields
+		// Submit button
 		Button submit = new Button("Submit");
 		submit.setPrefWidth(500);
 		gpforText.setConstraints(submit, 0, 5);
 		gpforText.getChildren().add(submit);
 		
 		//Change scene to home Screen
-		Button sceneButton = new Button("Go to Home Screen");		
+		Button sceneButton = new Button("Go Home");		
 		sceneButton.setFont(Font.font("Times New Roman", 20));
-		sceneButton.setAlignment(Pos.CENTER);
-		//gpforText.getChildren().add(sceneButton);
-		//gpforText.setConstraints(sceneButton, 0, 6);
+		sceneButton.setPrefWidth(175);
 		
-		gpforText.setAlignment(Pos.CENTER);
+		//Home button
+		Button goHome = new Button("Go home");
+		goHome.setPrefWidth(500);
+		gpforText.setConstraints(goHome, 0, 6);
+		gpforText.getChildren().add(goHome);
 		
-		//Layout 2
+		
+		
+		//Layout for record Your journey
 		VBox layout2 = new VBox(20);
-		layout2.getChildren().addAll(label2, gpforText, sceneButton);
+		layout2.getChildren().addAll(label2, gpforText);
 		layout2.setAlignment(Pos.TOP_CENTER);
 		layout2.setSpacing(100);
 		
+		//Journey Scene
 		Scene addJourney = new Scene(layout2, 1200, 800);
 		layout2.setStyle("-fx-background-color: #607658;");
 		
@@ -194,14 +205,14 @@ public class Main extends Application {
 
 		//-----------------History Screen------------------------
 		
-		//History Label
+		//History Title Label
 		Label history = new Label("History");
 		history.setStyle("-fx-text-fill: white;");
 		history.setFont(new Font("Times New Roman", 50));
 		history.setPadding(new Insets(75, 75, 0, 75));
 		
-		// History table
-		TableView table = new TableView<>();
+		// Table to show History of elements
+		TableView<Entry> table = new TableView<>();
 		
 		//Handles Table Columns
 		TableColumn<Entry, String> nameCol = new TableColumn<>("Name");
@@ -210,14 +221,19 @@ public class Main extends Application {
         dateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
         TableColumn<Entry, String> distCol = new TableColumn<>("Distance Hiked");
         distCol.setCellValueFactory(new PropertyValueFactory<>("distance"));
+        
         nameCol.setPrefWidth(200);
         dateCol.setPrefWidth(200);
         distCol.setPrefWidth(200);
         
+        
         //Table Attributes
         table.getColumns().addAll(nameCol, dateCol, distCol);
-        table.setMaxSize(600, 400);
+        table.setMaxSize(600, 600);
         table.setEditable(true);
+        
+        
+        //------------ Begin extraction from database file --------------
         
         //ArrayList of lines from database file
         ArrayList<String> line = new ArrayList<>();
@@ -232,29 +248,29 @@ public class Main extends Application {
         	//Create new Scanner object
         	Scanner in = new Scanner(new File("entries.txt")).useDelimiter("\t");
 
-        	//Loops through STRINGS in file
+        	//Loops through lines in file
         	while (in.hasNextLine()) {
 
-        		//Appends string to line ArrayList
+        		//Appends line to the line ArrayList
         		line.add(in.nextLine());
 				
         	}
         	
-        	//For loop splits line into 5 different
+        	//For loop splits line into 5 different Strings
         	for(int i = 0; i < line.size(); i++) {
 				
-        		//Split the line into words
+        		//Split the line into 5 strings with pertinent data
 				words = line.get(i).split("\t");
 				
-				
+				//Create new entry with the data from the file
 				Entry testt = new Entry();
-				
     			testt.setName(words[0]);
     			testt.setDate(words[1]);
     			testt.setDuration(words[2]);
     			testt.setDistance(words[3]);
     			testt.setJournal(words[4]);
-
+    			
+    			//Add the entry to the observable list & table
     			obsList.add(testt);
     			table.getItems().add(testt);
 
@@ -264,69 +280,104 @@ public class Main extends Application {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+         
+         
+         //------END------ End extraction from database file ------END-------
+         
         
-         //Delete Button
+        //Delete Button
         Button deleteButton = new Button("Delete");
+        deleteButton.setPrefWidth(175);
         deleteButton.setFont(Font.font("Times New Roman", 20));
         deleteButton.setAlignment(Pos.CENTER);
-		
+        
+        //View button
+        Button view = new Button("View");
+        view.setPrefWidth(175);
+        view.setFont(Font.font("Times New Roman", 20));
+        view.setAlignment(Pos.CENTER);
+        
+        //Hbox for buttons
         HBox hbox = new HBox(20);
-        hbox.getChildren().addAll(deleteButton, sceneButton);
+        hbox.getChildren().addAll(deleteButton, sceneButton, view);
         hbox.setAlignment(Pos.TOP_CENTER);
         
+        //History Screen layout
 		VBox history1 = new VBox(20);
 		history1.getChildren().addAll(history, table, hbox);
 		history1.setAlignment(Pos.TOP_CENTER);
 		history1.setSpacing(10);
 		
+		//History Scene
 		Scene viewHistory = new Scene(history1, 1200, 800);
 		history1.setStyle("-fx-background-color: #607658;");
 		
+		//--------END------History Screen-------END----------------------
 		
 		
-				
-		//--------END------History Screen-------------------------------------
+		
+		//----------------------View Entry ----------------
+		
+		//Vbox to show a detailed view of Selected Entry
+		VBox popupBox = new VBox(20);
+		popupBox.setAlignment(Pos.CENTER);
+		popupBox.setSpacing(20);
+		popupBox.setStyle("-fx-background-color: #607658;");
+		popupBox.setPrefWidth(500);
+		popupBox.setPrefHeight(500);
+		
+		//Scene acts as a popup
+		Scene pop = new Scene(popupBox, 1200, 800);
+		
+		//Back button
+		Button back = new Button("Back");
+		
+		//Add back button to Layout
+		popupBox.getChildren().add(back);
+		
+		//--------END------View Entry-------END----------------------
 		
 				
-				
-		//----------------------Button Actions----------------
+		
+		//----------------------Button Handlers----------------
 		
 		//Change scene to Add Journal entry
 		btn1.setOnAction(e -> primaryStage.setScene(addJourney));
 		
 		//Change scene to Add Home Screen
 		sceneButton.setOnAction(e -> primaryStage.setScene(homeScene));
+		goHome.setOnAction(e -> primaryStage.setScene(homeScene));
 		
-		//Change scene to View History
-		//btn2.setOnAction(e -> primaryStage.setScene(viewHistory));
+		//Change scene to View History and Refresh Table
 		btn2.setOnAction(new EventHandler<ActionEvent>() {
 			
 			@Override
 			public void handle(ActionEvent t) {
 				primaryStage.setScene(viewHistory);
-				table.getColumns().clear();
-				table.getColumns().addAll(nameCol, dateCol, distCol);
+				//table.getColumns().clear();
+				//table.getColumns().addAll(nameCol, dateCol, distCol);
 				}
 			});
 		
-		//Gather and input text fields into entry object
+		//Submit button handler
 		submit.setOnAction(new EventHandler<ActionEvent>() {
 			
 			@Override
 			public void handle(ActionEvent t) {
-				
-				
-				
+
 				// Entry( Name, Date, Duration, Distance, Entry)
 				String entryField = entry.getText();
 				Entry temp = new Entry(name.getText(), date.getText(), time.getText(), distance.getText(), entryField );
 				
-				if(isValidName(name.getText()) && isValidName(date.getText())
-						&& isValidName(time.getText()) &&  isValidName(distance.getText()) && isValidName(entryField)){
+				//If statement to validate user input
+				if(isValidStr(name.getText()) && isValidStr(date.getText())
+						&& isValidStr(time.getText()) &&  isValidStr(distance.getText()) && isValidStr(entryField)){
 					
+					//Add entry to table and observable list backup
 					table.getItems().add(temp);
 					obsList.add(temp);
 					
+					//Write new entry to database file
 					try {
 						FileOutputStream out = new FileOutputStream("entries.txt", true);
 						
@@ -350,17 +401,19 @@ public class Main extends Application {
 					alert.setHeaderText("Journal Entry Submitted");
 					alert.showAndWait();
 					
+					//Return to home screen upon successful entry
 					primaryStage.setScene(homeScene);
 				}
 				else {
-					//Successful Alert
+					//Failed Alert
 					Alert alert = new Alert(AlertType.INFORMATION);
 					alert.setTitle("Error");
-					alert.setContentText("Fill in all entries bruh");
+					alert.setContentText("All entries must be filled, and contain numbers, letters, or a backslash");
 					alert.setHeaderText("Invalid Input");
 					alert.showAndWait();
 				}
 				
+				//Clear form
 				name.clear();
 				date.clear();
 				distance.clear();
@@ -370,15 +423,17 @@ public class Main extends Application {
 
 			}
 			
-		});
+		}); // END Submit Button Handler
 		
-		
+		// Delete button handler
 		deleteButton.setOnAction(e -> {
+			
+			//Remove Selected item from table and observable list
 		    Object selectedItem = table.getSelectionModel().getSelectedItem();
 		    table.getItems().remove(selectedItem);
 		    obsList.remove(selectedItem);
 		    
-		    
+		    // Overwrite deleted entries
 		    try {
 				PrintWriter pw = new PrintWriter("entries.txt");
 				pw.close();
@@ -387,7 +442,7 @@ public class Main extends Application {
 				e1.printStackTrace();
 			}
 		    
-		    
+		    // Re-add the non-deleted entries
 		    for (int i = 0; i < obsList.size(); i++) {
 		    	
 		    	try {
@@ -409,10 +464,81 @@ public class Main extends Application {
 		    }
 		    
 		    
-		});
+		}); // END Delete Button Handler
 		
+		// View Button handler
+		view.setOnAction(new EventHandler<ActionEvent>() {
+			
+			// Shows a detailed view of a single entry
+			@Override
+			public void handle(ActionEvent event) {
+				//singleTable.getItems().add(selectedItem);
+				Entry viewE = table.getSelectionModel().getSelectedItem();
+		        
+		        Label viewName = new Label("Name: " + viewE.getName());
+		        Label viewDist = new Label("Distance: " + viewE.getDistance());
+		        Label viewDur = new Label("Duration: " + viewE.getDuration());
+		        Label viewDate = new Label("Date: " + viewE.getDate());
+		        Text viewEntry = new Text("Journal Entry:\n" + viewE.getJournal());
+		        
+		        viewEntry.wrappingWidthProperty().set(500);
+		        
+		        
+		        //Format Font
+		        viewName.setFont(Font.font("Times New Roman", 20));
+		        viewDist.setFont(Font.font("Times New Roman", 20));
+		        viewDur.setFont(Font.font("Times New Roman", 20));
+		        viewDate.setFont(Font.font("Times New Roman", 20));
+		        viewEntry.setFont(Font.font("Times New Roman", 20));
+		        viewName.setTextFill(Color.web("#ffffff"));
+		        viewDist.setTextFill(Color.web("#ffffff"));
+		        viewDur.setTextFill(Color.web("#ffffff"));
+		        viewDate.setTextFill(Color.web("#ffffff"));
+		        viewEntry.setFill(Color.WHITE);
+		        
+		        //Add the entry elements to the vBox
+		        popupBox.getChildren().add(0, viewEntry);
+		        popupBox.getChildren().add(0, viewDist);
+		        popupBox.getChildren().add(0, viewDur);
+		        popupBox.getChildren().add(0, viewDate);
+		        popupBox.getChildren().add(0, viewName);
+				
+		        //Change scene to the detailed entry view
+		        primaryStage.setScene(pop);
+		        
+		        /*
+		         
+		         
+		        if(popup.isShowing()) {
+		        	popup.hide();
+		        	popupBox.getChildren().clear();
+		        }
+		        else
+		        	popup.show(primaryStage);
+		        
+		        Button exit = new Button("Close");
+		        popupBox.getChildren().add(exit);
+		        
+		        exit.setOnAction(e -> {
+		        	
+		        	popup.hide();
+		        	popupBox.getChildren().clear();
+		        	
+		        });*/
+			}
+		}); // END View Button Handler
 		
-		//-----------------END---Button Actions----------------
+		//Back button handler
+		back.setOnAction(e -> {
+			
+			//Return to history scene, refresh the detailed view scene
+			primaryStage.setScene(viewHistory);
+			popupBox.getChildren().clear();
+			popupBox.getChildren().add(back);
+			
+		});// END back Button Handler
+		
+		//-----------------END---Button Handlers------END-------
 		
 		//Set default scene
 		primaryStage.setScene(homeScene);
